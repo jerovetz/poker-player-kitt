f3be95097febef8acf80185d4ba29fe26ef9b3ca
David Orban <david.orban@emarsys.com>
2015-10-02 10:05:57 +0200

    allways betting min

--- player.php
+++ player.php
@@ -6,10 +6,17 @@ class Player
 
     public function betRequest($game_state)
     {
+        $this->betMinimumRaise($game_state);
         return 0;
     }
 
     public function showdown($game_state)
     {
+
+    }
+
+    private function betMinimumRaise($game_state)
+    {
+        return $game_state['current_buy_in'] + $game_state['minimum_raise'];
     }
 }
