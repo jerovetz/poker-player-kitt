99f7bb0ede2f36df10f000f101e3c718247e7ac1
gmanner <gabor.manner@emarsys.com>
2015-10-02 13:46:08 +0200

    Rainman client

---
+++ rainman.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: gmanner
+ * Date: 02/10/15
+ * Time: 13:32
+ */
+class Rainman
+{
+
+
+    public static function fetch($cards)
+    {
+        return json_decode(file_get_contents('http://rainman.leanpoker.org/rank?cards=' . urlencode(json_encode($cards))));
+    }
+
+    public static function getRank($cards)
+    {
+        return self::fetch($cards)->rank;
+    }
+
+}
