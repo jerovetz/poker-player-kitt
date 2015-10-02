b6cd8e2c1c9f6bd812617a842a244d68e2f6b8bc
pvasarhelyi <pvasarhelyi@emarsys.com>
2015-10-02 12:02:51 +0200

    cardhelper generalized cardanalizer::isSuited

--- cardanalizer.php
+++ cardanalizer.php
@@ -6,7 +6,22 @@ class CardAnalizer
 
     public function isSuited(array $cards) 
     {
-        return $cards[0]->suit == $cards[1]->suit;
+        $suitsCount = $this->countSuit($cards);
+        $largestSuit = reset($suitsCount);
+
+        return $largestSuit >= 2;
+    }
+
+    public function countSuit(array $cards)
+    {
+        $suits = array_unique(array_map(function($cards) {
+            return $cards->suit;
+        }, $cards));
+
+        $suitsCount = array_count_values($suits);
+        arsort($suitsCount);
+
+        return $suitsCount;
     }
 
     public function isConnected(array $cards)
@@ -23,25 +38,15 @@ class CardAnalizer
     public function isHighPair(array $cards)
     {
         return $this->isPair($cards)
-            && $this->mapRankToValues($cards[0]->rank) >= 10;
+            && CardHelper::mapRankToValues($cards[0]->rank) >= 10;
     }
 
     public function rankDiff(array $cards)
     {
-        $firstRank = $this->mapRankToValues($cards[0]->rank);
-        $secondRank = $this->mapRankToValues($cards[1]->rank);
+        $firstRank = CardHelper::mapRankToValues($cards[0]->rank);
+        $secondRank = CardHelper::mapRankToValues($cards[1]->rank);
         return $firstRank - $secondRank;
     }
 
-    public function mapRankToValues($rank)
-    {
-        switch (strtoupper($rank))
-        {
-            case 'J': return 11;
-            case 'Q': return 12;
-            case 'K': return 13;
-            case 'A': return 14;
-            default: return $rank;
-        }
-    }
+
 }
