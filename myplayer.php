9bf935f7444d43c70033b9b154e8cd08f66c1157
gmanner <gabor.manner@emarsys.com>
2015-10-02 14:15:44 +0200

    MyPlayer getHand fix

--- myplayer.php
+++ myplayer.php
@@ -9,6 +9,14 @@
 class MyPlayer extends GameObject
 {
     public function getHand() {
-        return $this->hole_cards;
+        return $this->sortCards($this->hole_cards);
+    }
+
+    public function sortCards($cards)
+    {
+        usort($cards, function($a, $b) {
+            return (CardHelper::mapRankToValues($a->rank) < CardHelper::mapRankToValues($b->rank)) ? -1 : 1;
+        });
+        return $cards;
     }
 }
