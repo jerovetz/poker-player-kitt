b0b311fecac4b06b05f887aaace016510c605699
pvasarhelyi <pvasarhelyi@emarsys.com>
2015-10-02 13:49:56 +0200

    play when we have pairs!

--- decisions.php
+++ decisions.php
@@ -19,16 +19,16 @@ class Decisions
 
     public function shouldRaise(array $cards)
     {
+        if ($this->cardsAnalizer->isPair($cards)) {
+            return true;
+        }
+
         if ($this->isHeadsUp()){
             return true;
         }
 
         $activePlayers = $this->gameState->playersWithStatus('active');
         if (count($activePlayers) <= 3) {
-            return true;
-        }
-
-        if ($this->cardsAnalizer->isPair($cards)) {
             return true;
         }
 
