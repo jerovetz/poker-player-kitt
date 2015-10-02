dff8de1b4ce708fb3befffbb961a5396e4fc043c
David Orban <david.orban@emarsys.com>
2015-10-02 10:12:59 +0200

    betting only when there are 2 active players

--- player.php
+++ player.php
@@ -6,7 +6,12 @@ class Player
 
     public function betRequest($game_state)
     {
-        return $this->betMinimumRaise($game_state);
+        $activePlayers = $this->getActivePlayers($game_state);
+        if(count($activePlayers) == 2) {
+            $minBet = $this->betMinimumRaise($game_state);
+            return (int)$minBet;
+        }
+        return 0;
     }
 
     public function showdown($game_state)
@@ -17,5 +22,16 @@ class Player
     private function betMinimumRaise($game_state)
     {
         return $game_state['current_buy_in'] + $game_state['minimum_raise'];
+    }
+
+    private function getActivePlayers($game_state)
+    {
+        $activePlayers = array();
+        foreach ($game_state['players'] as $player) {
+            if ($player['status'] == 'active') {
+                $activePlayers[] = $player;
+            }
+        }
+        return $activePlayers;
     }
 }
